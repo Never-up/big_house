6048677ad6798f649bd6c17e889e2bc931dcad09
廖鼓楼 <gulouliao@gmail.com>
2021-05-28 15:33:31 +0800

    word2vecTrain.py  随便的记录

---
+++ Word2vecTrain.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pandas
+from elasticsearch import Elasticsearch
+
+'''
+    @user   : gl_mac
+    @file   : Word2vecTrain
+    @Author : gulou
+    @Data   : 2021/5/283:32 下午
+    @Decs   : 自己对此文件的描述
+    @email  : gulouliao@gmail.com
+    
+'''
+
+if __name__ == "__main__":
+    pass
