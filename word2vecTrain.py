cac39745543a4e5b97217554146279ca02773e28
廖鼓楼 <gulouliao@gmail.com>
2021-05-28 15:01:05 +0800

    word2vecTrain.py第一次小小的修改

---
+++ word2vecTrain.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from gensim.models import Word2Vec, KeyedVectors
+from elasticsearch import Elasticsearch
+from sanic.response import json
+import pandas
+import torch
+
+
+
+'''
+    @user   : gl_mac
+    @file   : word2vecTrain
+    @Author : gulou
+    @Data   : 2021/5/282:58 下午
+    @Decs   : 自己对此文件的描述
+    @email  : gulouliao@gmail.com
+    
+'''
+
+if __name__ == "__main__":
+    print('你想去堆雪人吗')
